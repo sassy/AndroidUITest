017f1bd1b1a996b7091af0be10d8c311115d38cf
sassy watanabe <sassy.watanabe@gmail.com>
2011-12-24 14:41:45 +0900

    add Spinner and EditText test case.

--- HelloAndroid/src/com/blogspot/sassylog/helloandroid/HelloAndroidActivity.java
+++ HelloAndroid/src/com/blogspot/sassylog/helloandroid/HelloAndroidActivity.java
@@ -4,11 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class HelloAndroidActivity extends Activity {
     private int x = 0;
     private int y = 0;
+    private String selectedItem;
+    private String sendData;
     
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +35,34 @@ public class HelloAndroidActivity extends Activity {
                 startActivityForResult(intent, 100);
             }
         });
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adapter.add("dog");
+        adapter.add("cat");
+        adapter.add("bird");
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = (String) parent.getSelectedItem();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selectedItem = null;
+            }
+        });
+        
+        Button sendButton = (Button)findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edit = (EditText) v.getRootView().findViewById(R.id.editer);
+                sendData = edit.getText().toString();
+            }
+        });
     }
-    
+
     @Override
     protected void onStart() {
         y = 1;
@@ -48,5 +81,13 @@ public class HelloAndroidActivity extends Activity {
     
     public int getY() {
         return y;
+    }
+
+    public String getSelectedItem() {
+        return selectedItem;
+    }
+
+    public String getSendData() {
+        return sendData;
     }
 }
--- HelloAndroidTest/src/com/blogspot/sassylog/helloandroid/test/HelloAndroidActivityTest.java
+++ HelloAndroidTest/src/com/blogspot/sassylog/helloandroid/test/HelloAndroidActivityTest.java
@@ -5,7 +5,10 @@ import com.blogspot.sassylog.helloandroid.HelloAndroidActivity;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
+import android.view.KeyEvent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
@@ -46,10 +49,115 @@ public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<H
         assertEquals(1, activity.getX());
         
     }
-    
+
+    /* use touchUtils */
     public void testButton3() throws Exception {
-        final Button button = (Button)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.button);
+        Button button = (Button)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.button);
         TouchUtils.clickView(this, button);
         assertEquals(1, activity.getX());
     }
+
+    public void testSpinner0() throws Exception {
+        Spinner spinner = (Spinner)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.spinner);
+        assertTrue(spinner.getOnItemSelectedListener() != null);
+        assertTrue(spinner.getAdapter() != null);
+        assertEquals(3, spinner.getAdapter().getCount());
+    }
+    
+    public void testSpinner1() throws Exception {
+        final Spinner spinner = (Spinner)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.spinner);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                spinner.requestFocus();
+            } 
+        });
+        instrumentation.waitForIdleSync();
+        assertEquals("dog", spinner.getSelectedItem());
+        assertEquals("dog", spinner.getItemAtPosition(spinner.getSelectedItemPosition()));
+        assertEquals("dog", activity.getSelectedItem());
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                spinner.requestFocus();
+                spinner.setSelection(1);
+            } 
+        });
+        instrumentation.waitForIdleSync();
+        assertEquals("cat", spinner.getSelectedItem());
+        assertEquals("cat", spinner.getItemAtPosition(spinner.getSelectedItemPosition()));
+        assertEquals("cat", activity.getSelectedItem());
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                spinner.requestFocus();
+                spinner.setSelection(2);
+            } 
+        });
+        instrumentation.waitForIdleSync();
+        assertEquals("bird", spinner.getSelectedItem());
+        assertEquals("bird", spinner.getItemAtPosition(spinner.getSelectedItemPosition()));
+        assertEquals("bird", activity.getSelectedItem());
+    }
+
+    public void testSpinner2() throws Exception {
+        final Spinner spinner = (Spinner)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.spinner);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                spinner.requestFocus();
+            }
+        });
+        sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+        sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        assertEquals("cat", spinner.getSelectedItem());
+        assertEquals(spinner.getSelectedItem(), activity.getSelectedItem());
+    }
+
+    /* use TouchUtils */
+    public void testSpinner3() throws Exception {
+        Spinner spinner = (Spinner)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.spinner);
+        TouchUtils.clickView(this, spinner);
+        sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+        sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        assertEquals(spinner.getSelectedItem(), activity.getSelectedItem());
+    }
+
+    public void testEditText0() throws Exception {
+        EditText edit = (EditText)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.editer);
+        assertTrue(edit.getText().toString().equals(""));
+    }
+
+    public void testEditText1() throws Exception {
+        final EditText edit = (EditText)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.editer);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                edit.requestFocus();
+            }
+        });
+        sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        sendKeys(KeyEvent.KEYCODE_F);
+        sendKeys(KeyEvent.KEYCODE_O);
+        sendKeys(KeyEvent.KEYCODE_X);
+        sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+        sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        assertEquals("fox", activity.getSendData());
+    }
+
+    /* use TouchUtils */
+    public void testEditText2() throws Exception {
+        EditText edit = (EditText)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.editer);
+        Button button = (Button)activity.findViewById(com.blogspot.sassylog.helloandroid.R.id.send);
+        TouchUtils.clickView(this, edit);
+        sendKeys(KeyEvent.KEYCODE_C);
+        sendKeys(KeyEvent.KEYCODE_O);
+        sendKeys(KeyEvent.KEYCODE_W);
+        TouchUtils.clickView(this, button);
+        assertEquals("cow", activity.getSendData());
+    }
+
 }
